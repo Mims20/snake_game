2d9fc5e4f4395f862ff402ba297804e8fbaf0c22
Mims20 <psterlings@gmail.com>
2021-08-17 18:54:44 -0400

    TODO update

--- todo.py
+++ todo.py
@@ -1,16 +1,20 @@
-1. #TODO
+# TODO 1:
+"""Create the snake body and make it move"""
 
+# TODO 2:
+'''Place snake food randomly on screen'''
 
-2. # TODO
+# TODO 3:
+'''Snake should be able to grow longer after picking food up'''
 
+# TODO 4:
+'''Create and update scoreboard of how much food snake picks
+   Current score and all time highest score should both be on screen
+'''
 
-3. # TODO
-
-
-4. # TODO
-
-
-5. # TODO
-
-
-6. # TODO
+# TODO 5:
+'''Detect collision of snake with walls'''
+# TODO 6:
+'''Detect collision of snake with its own body
+   Save highest score of the game to a file locally
+'''
