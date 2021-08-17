c8d5561370b7de205316ccab1af32723bc59ce47
Mims20 <psterlings@gmail.com>
2021-08-17 18:43:38 -0400

    modified code to be able to save high scores to a file

--- main.py
+++ main.py
@@ -35,14 +35,14 @@ while is_game_on:
 
     # detect collision with walls
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
-        is_game_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # detect collision with tail/own body
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            is_game_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+
 
 
 screen.exitonclick()
